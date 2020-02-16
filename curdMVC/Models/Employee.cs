9a6576569e6d77750d2638875157e85ee0209160
unknown <jideshvaidya@gmail.com>
2020-02-16 12:57:33 +0545

    first commit1

---
+++ curdMVC/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace curdMVC.Models
+{
+    public class Employee
+    {
+        public int empid { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public string Pin_code { get; set; }
+        public string Designation { get; set; }
+    }
+}
